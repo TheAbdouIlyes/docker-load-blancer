ddbf228584283afcb393b9ed2b4b00465d05a8c9
Madani Abderrahmane Ilyes <ab.madani@univ-alger.dz>
2025-12-14 08:02:37 +0100

    cleanning

--- fix-admin.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM admins WHERE username='admin';
-INSERT INTO admins (username, password, email) VALUES ('admin', '$2b$10$qt1v9dlVNQewX5oQSUhz6uJECv4kI7rrO7yS2B1FJrEx.hslTUX9q', 'admin@voting.local');
-SELECT id, username, password FROM admins;
